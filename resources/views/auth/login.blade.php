5669f6a9ea83e1cfc8d8187bd1bae57071518148
Sandi625 <110502593+Sandi625@users.noreply.github.com>
2024-08-28 22:39:15 +0700

    commit 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,33 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+        body {
+            background-image: url('{{ asset('img/1ijen.JPG') }}'); /* Path to your background image */
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .login-card {
+            background-color: rgba(255, 255, 255, 0.7); /* Increased transparency for the card */
+            padding: 2rem;
+            border-radius: 0.5rem;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
     <title>Login</title>
 </head>
 <body>
-    <div class="container py-5">
-        <div class="w-50 center border rounded px-3 py-3 mx-auto">
-        <h1>Login</h1>
+    <div class="login-card w-50">
+        <h1 class="mb-4">Login</h1>
         @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
-                @foreach ($errors->all() as $item )
-                <li>{{$item}}</li>
-
+                @foreach ($errors->all() as $item)
+                <li>{{ $item }}</li>
                 @endforeach
             </ul>
         </div>
-
         @endif
         <form action="" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" value="{{old('email')}}" name="email" class="form-control">
+                <input type="email" value="{{ old('email') }}" name="email" class="form-control" id="email">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control" id="password">
             </div>
             <div class="mb-3 d-grid">
                 <button name="submit" type="submit" class="btn btn-primary">Login</button>
@@ -40,6 +55,6 @@
         {{-- <p class="mb-0">
             <a href="/register" class="text-center">Register a new membership</a> --}}
     </div>
-    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-mQ93vA8snR3YmJvW/X4wihRIQq5iE2U+pL6cI8N6ekD4t1Q7ihY6u6d6m4Qc9P2" crossorigin="anonymous"></script>
 </body>
 </html>
